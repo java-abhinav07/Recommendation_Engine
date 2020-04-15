1ce9723ac0c1ac07b71dbb78876b621f69eb0e72
vkartik2k <vkartik2k@gmail.com>
2020-04-16 00:13:55 +0530

    parallax working

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,8 @@
 $(document).ready(() => {
-    console.log("Hello World")
+    console.log("Hello ji")
+    $(window).on('scroll',function() {
+        let ratio = ($(window).scrollTop())/($(document).height() - $(window).height())
+        $('#under').css('top', String(ratio*($(window).height()) + 100) + 'px')
+        console.log()
+    })
 })
