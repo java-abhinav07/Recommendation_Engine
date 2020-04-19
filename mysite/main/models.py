f6ea8a37bbf8d70570146a552be42fc1eaf887bc
java-abhinav07 <java.abhinav99@gmail.com>
2020-04-19 19:24:49 +0530

    added models, admin stuff in progress

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,3 +1,62 @@
 from django.db import models
-
+#from custom.fields import SeparatedValuesField
 # Create your models here.
+
+
+
+class Movies(models.Model):
+    """
+    fields: title, year, genre, 
+            Director, Stars, IMDb, 
+            Oscar, Duration
+
+    """
+    movie_title = models.CharField(max_length=255)
+    movie_year = models.IntegerField()
+    movie_genre = models.TextField()
+    movie_director = models.CharField(max_length=255)
+    movie_stars = models.TextField()
+    movie_rating = models.FloatField()
+    movie_oscar = models.CharField(max_length=10)
+    movie_duration = models.IntegerField()
+
+
+    def __str__(self):
+        return self.movie_title
+
+
+
+class Books(models.Model):
+    """
+    fields: title, author, rating, img_src
+
+    """
+    book_title = models.CharField(max_length=255)
+    book_author = models.CharField(max_length=255)
+    book_rating = models.FloatField()
+    book_img_src = models.TextField()
+
+    def __str__(self):
+        return self.book_title
+
+
+
+class TVShows(models.Model):
+    """
+    fields: title, year, genre, Stars, rating, duration
+    """
+
+    show_title = models.CharField(max_length=255)
+    show_year = models.IntegerField()
+    show_genre = models.TextField()
+    show_stars = models.TextField()
+    show_rating = models.FloatField()
+
+
+    def __str__(self):
+        return self.show_title
+
+
+
+    
+
