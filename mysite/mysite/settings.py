d018dbf38091bf6f53e7a0df6b8782c33c36ad74
kajal1511 <gupta.kajal15@gmail.com>
2020-04-27 19:03:56 +0530

    django filters

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main.apps.MainConfig', 
-    'django_filters'
+    # 'django_filters'
 ]
 
 MIDDLEWARE = [
