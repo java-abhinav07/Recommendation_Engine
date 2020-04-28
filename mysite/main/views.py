923e94598828bc535c5e52a8022ece0a2f4255a9
java-abhinav07 <java.abhinav99@gmail.com>
2020-04-29 03:05:19 +0530

    data loading on wheel

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -9,30 +9,34 @@ from django.core.serializers import serialize
 
 # Create your views here.
 
+class mydict(dict):
+    def __str__(self):
+        return json.dumps(self)
+
 def is_valid_queryparam(param):
     print(param)
-    return param != '' and param
+    return param != "" and param
 
 
 
 def index(request):
     #data needed for index template: index.html
     #3 buttons for url to movies, books and tv shows
-    return render(request, 'index.html')
+    return render(request, "index.html")
 
 
 def movies(request):
     
     movies_query = Movie.objects.all()
 
-    title_query = request.GET.get('title_contains') # query param
-    year_query = request.GET.get('year_contains') 
-    genre_query = request.GET.get('genre_contains') 
-    director_query = request.GET.get('director_contains') 
-    stars_query = request.GET.get('stars_contains') 
-    rating_query = request.GET.get('rating_contains')
-    oscar_query  = request.GET.get('oscar_contains')
-    duration_query = request.GET.get('duration_contains')
+    title_query = request.GET.get("title_contains") # query param
+    year_query = request.GET.get("year_contains") 
+    genre_query = request.GET.get("genre_contains") 
+    director_query = request.GET.get("director_contains") 
+    stars_query = request.GET.get("stars_contains") 
+    rating_query = request.GET.get("rating_contains")
+    oscar_query  = request.GET.get("oscar_contains")
+    duration_query = request.GET.get("duration_contains")
 
 
     if is_valid_queryparam(title_query):
@@ -49,39 +53,39 @@ def movies(request):
         movies_query = movies_query.filter(rating__gte=rating_query)
     if is_valid_queryparam(oscar_query):
         movies_query = movies_query.filter(oscar__icontains=oscar_query)
-    if duration_query != '' and duration_query:
+    if duration_query != "" and duration_query:
         movies_query = movies_query.filter(duration__icontains=duration_query)
 
     
 
-    movies_list = movies_query.values_list('id', flat=True)
+    movies_list = movies_query.values_list("id", flat=True)
     random.shuffle(list(movies_list))
 
 
     movies_random = random.sample(list(movies_list), min(len(movies_list), 10))
     movies_query = movies_query.filter(id__in=movies_random)
 
-    movies = list(dict() for i in range(len(movies_query)))
+    movies = list(list([] for _ in range(2)) for _ in range(len(movies_query)))
+    #print(movies)
     print(len(movies))
     
     for i in range(len(list(movies_query.values()))):
-        movies[i]["label"] = list(movies_query.values())[i]['title']
-        movies[i]["value"] = 1 
-
-    movies = json.dumps(movies, cls=DjangoJSONEncoder)
+        movies[i][0] = "label", list(movies_query.values())[i]["title"]
+        movies[i][1] = "value", 1
+        movies[i] = mydict(movies[i])
+    movies = json.dumps(movies)
+    print(movies)
     
-    y = json.loads(movies)
-    print(y)
 
-    return render(request, 'movies.html', {'movies':movies})
+    return render(request, "movies.html", {"movies":movies})
     
 
 def books(request):
     books = Book.objects.all()
 
-    title_query = request.GET.get('title_contains')
-    author_query = request.GET.get('author_contains')
-    rating_query = request.GET.get('rating_contains')
+    title_query = request.GET.get("title_contains")
+    author_query = request.GET.get("author_contains")
+    rating_query = request.GET.get("rating_contains")
 
 
     if is_valid_queryparam(title_query):
@@ -94,19 +98,19 @@ def books(request):
     books = random.sample(books, min(books.objects.count(), 10))
 
 
-    return render(request, 'books.html', {'books': books})
+    return render(request, "books.html", {"books": books})
 
 
 
 def shows(request):
     shows = TVShow.objects.all()
 
-    title_query = request.GET.get('title_contains')
-    year_query = request.GET.get('year_contains')
-    genre_query = request.GET.get('genre_contains') 
-    stars_query = request.GET.get('stars_contains')
-    rating_query = request.GET.get('rating_contains')
-    duration_query = request.GET.get('duration_contains')
+    title_query = request.GET.get("title_contains")
+    year_query = request.GET.get("year_contains")
+    genre_query = request.GET.get("genre_contains") 
+    stars_query = request.GET.get("stars_contains")
+    rating_query = request.GET.get("rating_contains")
+    duration_query = request.GET.get("duration_contains")
 
 
     if is_valid_queryparam(title_query):
@@ -126,4 +130,4 @@ def shows(request):
 
 
 
-    return render(request, 'shows.html', {'shows': shows})
+    return render(request, "shows.html", {"shows": shows})
