eea2beaccac10be3407adcf5ef4e7e21aeaf6087
java-abhinav07 <java.abhinav99@gmail.com>
2020-04-28 07:40:36 +0530

    Revert "exp"
    This reverts commit 70fdb761a16dfdc384622ee938f9e4e188284e67, reversing
    changes made to 16506cfd9f372710914aec5d9b2a96653cf2f009.

--- mysite/static/movies.js
+++ mysite/static/movies.js
@@ -6,9 +6,7 @@ var padding = { top: 5, right: 50, bottom: 0, left: 0 },
     oldrotation = 0,
     picked = 100000,
     oldpick = [],
-    // color = d3.scale.category20();//category20c()
-    color = d3.scale.ordinal().range(["#834747","#995650","#986562","#AB716D","#834747","#995650","#986562","#AB716D","#834747","#995650","#986562","#AB716D"]);
-
+    color = d3.scale.category20();//category20c()
 var data = [
     { "label": "DELL LAPTOP", "value": 1 }, // padding
     { "label": "IMAC PRO", "value": 1}, //font-family
