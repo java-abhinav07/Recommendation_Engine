a22e0e9f31e97ae08431135b4e3d07f42fc114d1
java-abhinav07 <java.abhinav99@gmail.com>
2020-04-29 03:33:40 +0530

    changed views for all categories

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -21,9 +21,95 @@ def is_valid_queryparam(param):
 
 def index(request):
     #data needed for index template: index.html
-    #3 buttons for url to movies, books and tv shows
+    #3 buttons for url to shows, books and tv shows
     return render(request, "index.html")
 
+
+def books(request):
+    
+    books_query = Book.objects.all()
+
+    title_query = request.GET.get("title_contains") # query param
+    author_query = request.GET.get("author_contains") 
+    rating_query = request.GET.get("rating_contains")
+
+
+    if is_valid_queryparam(title_query):
+        books_query = books_query.filter(title__icontains=title_query)
+    if is_valid_queryparam(author_query):
+        books_query = books_query.filter(director__icontains=author_query)
+    if is_valid_queryparam(rating_query):
+        books_query = books_query.filter(rating__gte=rating_query)
+
+    
+
+    books_list = books_query.values_list("id", flat=True)
+    random.shuffle(list(books_list))
+
+
+    books_random = random.sample(list(books_list), min(len(books_list), 10))
+    books_query = books_query.filter(id__in=books_random)
+
+    books = list(list([] for _ in range(2)) for _ in range(len(books_query)))
+    #print(books)
+    print(len(books))
+    
+    for i in range(len(list(books_query.values()))):
+        books[i][0] = "label", list(books_query.values())[i]["title"]
+        books[i][1] = "value", 1
+        books[i] = mydict(books[i])
+    books = json.dumps(books)
+    print(books)
+    
+
+    return render(request, "books.html", {"books":books})
+def shows(request):
+    
+    shows_query = TVShow.objects.all()
+
+    title_query = request.GET.get("title_contains") # query param
+    year_query = request.GET.get("year_contains") 
+    genre_query = request.GET.get("genre_contains") 
+    stars_query = request.GET.get("stars_contains") 
+    rating_query = request.GET.get("rating_contains")
+    duration_query = request.GET.get("duration_contains")
+
+
+    if is_valid_queryparam(title_query):
+        shows_query = shows_query.filter(title__icontains=title_query)
+    if is_valid_queryparam(year_query):
+        shows_query = shows_query.filter(year__gte=year_query)
+    if is_valid_queryparam(genre_query):
+        shows_query = shows_query.filter(genre__icontains=genre_query)
+    if is_valid_queryparam(stars_query):
+        shows_query = shows_query.filter(stars__icontains=stars_query)
+    if is_valid_queryparam(rating_query):
+        shows_query = shows_query.filter(rating__gte=rating_query)
+    if duration_query != "" and duration_query:
+        shows_query = shows_query.filter(duration__icontains=duration_query)
+
+    
+
+    shows_list = shows_query.values_list("id", flat=True)
+    random.shuffle(list(shows_list))
+
+
+    shows_random = random.sample(list(shows_list), min(len(shows_list), 10))
+    shows_query = shows_query.filter(id__in=shows_random)
+
+    shows = list(list([] for _ in range(2)) for _ in range(len(shows_query)))
+    #print(shows)
+    print(len(shows))
+    
+    for i in range(len(list(shows_query.values()))):
+        shows[i][0] = "label", list(shows_query.values())[i]["title"]
+        shows[i][1] = "value", 1
+        shows[i] = mydict(shows[i])
+    shows = json.dumps(shows)
+    print(shows)
+    
+
+    return render(request, "shows.html", {"shows":shows})
 
 def movies(request):
     
@@ -78,56 +164,5 @@ def movies(request):
     
 
     return render(request, "movies.html", {"movies":movies})
-    
-
-def books(request):
-    books = Book.objects.all()
-
-    title_query = request.GET.get("title_contains")
-    author_query = request.GET.get("author_contains")
-    rating_query = request.GET.get("rating_contains")
 
 
-    if is_valid_queryparam(title_query):
-        books = books.filter(title__icontains=title_query)
-    if is_valid_queryparam(author_query):
-        books = books.filter(author__icontains=author_query)
-    if is_valid_queryparam(rating_query):
-        books = books.filter(rating__gte=rating_query)
-
-    books = random.sample(books, min(books.objects.count(), 10))
-
-
-    return render(request, "books.html", {"books": books})
-
-
-
-def shows(request):
-    shows = TVShow.objects.all()
-
-    title_query = request.GET.get("title_contains")
-    year_query = request.GET.get("year_contains")
-    genre_query = request.GET.get("genre_contains") 
-    stars_query = request.GET.get("stars_contains")
-    rating_query = request.GET.get("rating_contains")
-    duration_query = request.GET.get("duration_contains")
-
-
-    if is_valid_queryparam(title_query):
-        movies = movies.filter(title__icontains=title_query)
-    if is_valid_queryparam(year_query):
-        movies = movies.filter(year__icontains=year_query)
-    if is_valid_queryparam(genre_query):
-        movies = movies.filter(genre__icontains=genre_query)
-    if is_valid_queryparam(stars_query):
-        movies = movies.filter(stars__icontains=stars_query)
-    if is_valid_queryparam(rating_query):
-        movies = movies.filter(rating__gte=rating_query)
-
-
-    shows = random.sample(shows, min(shows.objects.count(), 10))
-
-
-
-
-    return render(request, "shows.html", {"shows": shows})
