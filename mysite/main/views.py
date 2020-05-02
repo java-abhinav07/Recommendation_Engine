eee0bf518d3278bb6d3c5fc7e560d4e332d7cb87
kajal1511 <gupta.kajal15@gmail.com>
2020-05-02 21:23:21 +0530

    Revert "Merge branch 'master' of https://github.com/java-abhinav07/Recommendation_Engine"
    This reverts commit a8038570226def6760848982b0601b17397d5b9e, reversing
    changes made to 4a61b581cc5cd42dcf67bd905d5e22b699d5fc50.

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -5,7 +5,6 @@ import random
 import json
 from django.core.serializers.json import DjangoJSONEncoder
 from django.core.serializers import serialize
-from django.db.models import Q, Count
 #from .filters import MovieFilter, BookFilter, ShowFilter
 
 # Create your views here.
@@ -99,7 +98,7 @@ def shows(request):
     if is_valid_queryparam(rating_query):
         shows_query = shows_query.filter(rating__gte=rating_query)
     if duration_query != "" and duration_query:
-        shows_query = shows_query.filter(duration__lte=duration_query)
+        shows_query = shows_query.filter(duration__icontains=duration_query)
 
     
 
@@ -133,7 +132,7 @@ def movies(request):
     
     movies_query = Movie.objects.all()
 
-    #title_query = request.GET.get("title_contains") # query param
+    title_query = request.GET.get("title_contains") # query param
     year_query = request.GET.get("year_contains") 
     genre_query = request.GET.get("genre_contains") 
     director_query = request.GET.get("director_contains") 
@@ -141,27 +140,24 @@ def movies(request):
     rating_query = request.GET.get("rating_contains")
     oscar_query  = request.GET.get("oscar_contains")
     duration_query = request.GET.get("duration_contains")
-    title_or_stars_or_director_query = request.GET.get("title_or_stars_or_director_contains")
 
 
-    if is_valid_queryparam(title_or_stars_or_director_query):
-        movies_query = movies_query.filter(Q(title__icontains=title_or_stars_or_director_query) | Q(stars__icontains= title_or_stars_or_director_query) | Q(director__icontains = title_or_stars_or_director_query))
-    elif is_valid_queryparam(genre_query):
-        movies_query = movies_query.filter(genre__icontains=genre_query)
-    elif is_valid_queryparam(year_query):
+    if is_valid_queryparam(title_query):
+        movies_query = movies_query.filter(title__icontains=title_query)
+    if is_valid_queryparam(year_query):
         movies_query = movies_query.filter(year__gte=year_query)
-    elif is_valid_queryparam(stars_query):
-        movies_query = movies_query.filter(stars__icontains=stars_query)
-    elif is_valid_queryparam(director_query):
+    if is_valid_queryparam(genre_query):
+        movies_query = movies_query.filter(genre__icontains=genre_query)
+    if is_valid_queryparam(director_query):
         movies_query = movies_query.filter(director__icontains=director_query)
-    elif is_valid_queryparam(rating_query):
+    if is_valid_queryparam(stars_query):
+        movies_query = movies_query.filter(stars__icontains=stars_query)
+    if is_valid_queryparam(rating_query):
         movies_query = movies_query.filter(rating__gte=rating_query)
-    elif is_valid_queryparam(oscar_query):
+    if is_valid_queryparam(oscar_query):
         movies_query = movies_query.filter(oscar__icontains=oscar_query)
-    elif duration_query != "" and duration_query:
-        movies_query = movies_query.filter(duration__lte=int(duration_query))
-
-
+    if duration_query != "" and duration_query:
+        movies_query = movies_query.filter(duration__icontains=duration_query)
 
     
 
@@ -227,8 +223,7 @@ def movies(request):
         movies_details[i] = mydict(movies_details[i])
 
     movies_details = json.dumps(movies_details)    
-    #print(movies_details[8])
-
+    print(movies_details[8])
 
     return render(request, "movies.html", {"movies":movies, "movies_details":movies_details, "raw_details":raw_details})
 
