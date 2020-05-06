841c309819043c0bb2790f34f10559ff251a2f96
kajal1511 <gupta.kajal15@gmail.com>
2020-05-06 20:49:04 +0530

    arrows

--- mysite/static/index.js
+++ mysite/static/index.js
@@ -6,3 +6,17 @@ $(document).ready(() => {
         console.log()
     })
 })
+
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    } 
+  });
+});
